9447b5d4dfa41e2cf84c0a4e9ee81cff62b1022d
David Thomason <911671+dtom90@users.noreply.github.com>
2025-10-13 19:56:14 -0400

    fix dat scroll like a real g thang

--- components/NarrowView.tsx
+++ components/NarrowView.tsx
@@ -4,7 +4,7 @@ import { ScrollView, View } from 'react-native';
 export function NarrowView({ children }: { children: React.ReactNode }) {
   return (
     <View className="flex-1 w-full items-center" style={{ backgroundColor: Colors.background }}>
-      <View className='max-w-3xl w-full p-4'>
+      <View className='max-w-3xl w-full p-4 flex-1'>
         <ScrollView
           className="flex-1"
           showsVerticalScrollIndicator={false}
