692564198d083a71f763fe9c1750be21ef4ef258
David Thomason <911671+dtom90@users.noreply.github.com>
2026-03-01 18:51:16 -0500

    deletes add exercise form

--- app/(tabs)/workouts/index.tsx
+++ app/(tabs)/workouts/index.tsx
@@ -1,11 +1,9 @@
 import { NarrowView } from '@/components/common/NarrowView';
-import { AddExerciseForm } from '@/components/workouts/list/AddExerciseForm';
 import { ExerciseList } from '@/components/workouts/list/ExerciseList';
 
 export default function WorkoutsScreen() {
   return (
     <NarrowView>
-      <AddExerciseForm />
       <ExerciseList />
     </NarrowView>
   );
